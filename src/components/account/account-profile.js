6e5bb5a760f52149fc82e34dac6d75f9d21a0d8b
nghia0111 <20521659@gm.uit.edu.vn>
2023-02-02 21:48:00 +0700

    Update account-profile.js

--- src/components/account/account-profile.js
+++ src/components/account/account-profile.js
@@ -11,11 +11,7 @@ import {
 
 const user = {
   avatar: '/static/images/avatars/avatar_6.png',
-  city: 'Los Angeles',
-  country: 'USA',
-  jobTitle: 'Senior Developer',
   name: 'Katarina Smith',
-  timezone: 'GTM-7'
 };
 
 export const AccountProfile = (props) => (
