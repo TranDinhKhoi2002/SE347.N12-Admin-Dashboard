c1ce8abc621994c56bfc59f9fce908dcf8493a2a
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-02-02 10:55:29 +0700

    print number with commas

--- src/utils/printNumerWithCommas.js
+++ src/utils/printNumerWithCommas.js
@@ -1,3 +1,3 @@
 export function printNumberWithCommas(number) {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
