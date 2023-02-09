66ba1d1ff350861c988fd3d901aa17ef1a382e30
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-02-09 11:23:50 +0700

    translate 404 page to Vietnamese

--- src/pages/404.js
+++ src/pages/404.js
@@ -26,11 +26,10 @@ const Page = () => (
           }}
         >
           <Typography align="center" color="textPrimary" variant="h1">
-            404: The page you are looking for isn’t here
+            404: Không tìm thấy trang
           </Typography>
           <Typography align="center" color="textPrimary" variant="subtitle2">
-            You either tried some shady route or you came here by mistake. Whichever it is, try
-            using the navigation
+            Trang bạn tìm kiếm không tồn tại, vui lòng thử lại
           </Typography>
           <Box sx={{ textAlign: "center" }}>
             <img
@@ -51,7 +50,7 @@ const Page = () => (
               sx={{ mt: 3 }}
               variant="contained"
             >
-              Go back to dashboard
+              Dashboard
             </Button>
           </NextLink>
         </Box>
